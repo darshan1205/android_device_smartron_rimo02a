e73daa366fe4dcb43e371034b829791711d3c497
Luca Stefani <luca020400@lineageos.org>
2017-08-18 11:39:28 +0200

    rimo02a: init: Update for O
    Change-Id: I635dc8d27f92f9a2fa83da62c2f32e48bd2c7680

--- init/init_rimo02a.cpp
+++ init/init_rimo02a.cpp
@@ -2,6 +2,7 @@
    Copyright (c) 2015, The Linux Foundation. All rights reserved.
    Copyright (C) 2016 The CyanogenMod Project.
    Copyright (C) 2017 The CyanogenMod Project.
+   Copyright (C) 2018 The LineageOS Project.
 
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
@@ -33,13 +34,15 @@
 #include <stdlib.h>
 #include <sys/sysinfo.h>
 
+#include <android-base/file.h>
+#include <android-base/properties.h>
 #include <android-base/strings.h>
 
 #include "vendor_init.h"
 #include "property_service.h"
-#include "log.h"
-#include "util.h"
 
+using android::base::GetProperty;
+using android::base::ReadFileToString;
 using android::base::Trim;
 
 static void init_alarm_boot_properties()
@@ -48,10 +51,10 @@ static void init_alarm_boot_properties()
     char const *power_off_alarm_file = "/persist/alarm/powerOffAlarmSet";
     std::string boot_reason;
     std::string power_off_alarm;
-    std::string reboot_reason = property_get("ro.boot.alarmboot");
+    std::string reboot_reason = GetProperty("ro.boot.alarmboot", "");
 
-    if (read_file(boot_reason_file, &boot_reason)
-            && read_file(power_off_alarm_file, &power_off_alarm)) {
+    if (ReadFileToString(boot_reason_file, &boot_reason)
+            && ReadFileToString(power_off_alarm_file, &power_off_alarm)) {
         /*
          * Setup ro.alarm_boot value to true when it is RTC triggered boot up
          * For existing PMIC chips, the following mapping applies
