767af22dea29bfd94a66f2cdf9953e514a8294b7
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2017-09-12 23:56:49 +0530

    rimo02a: Build fingerprintd from source
    Signed-off-by: Darshan Manjrekar <darshanmanjrekar1205@gmail.com>

--- device.mk
+++ device.mk
@@ -85,6 +85,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=480
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    fingerprintd
+
 # IRQ
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf \
