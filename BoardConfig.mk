25258fc188e258dcd675fffff626e86de9c2a14e
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2017-09-12 20:38:07 +0530

    rimo02a: Add basic platform, arch & bootloader config
    Signed-off-by: Darshan Manjrekar <darshanmanjrekar1205@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,5 +16,26 @@
 
 DEVICE_PATH := device/smartron/rimo02a
 
+# Platform
+TARGET_BOARD_PLATFORM := msm8952
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno510
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := MSM8952
+
 # Inherit the proprietary files
 include vendor/smartron/rimo02a/BoardConfigVendor.mk
