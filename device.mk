70a8b26025d9c98f51401ef75d5689a4037744af
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2017-09-12 22:42:42 +0530

    rimo02a: Add bluetooth config
    Signed-off-by: Darshan Manjrekar <darshanmanjrekar1205@gmail.com>

--- device.mk
+++ device.mk
@@ -50,6 +50,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.audio.pro.xml:system/etc/permissions/android.hardware.audio.pro.xml
 
+# Bluetooth
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
+    frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     Snap
