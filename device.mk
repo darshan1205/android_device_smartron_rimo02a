a368c1e858d6e3755452a355f99893729d9a5ee7
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-03-25 15:59:27 +0800

    rimo02a: Import gps oss code for Oreo from caf
    * tag LA.BR.1.3.7-02010-8976.0
    * upstream sources repos:
     - https://source.codeaurora.org/quic/la/platform/hardware/qcom/gps
     - https://source.codeaurora.org/quic/la/platform/vendor/qcom-opensource/location
    * update gps configs from coolpad oreo rom
    
    Change-Id: Id76d9f4a35b4b17d2881ee4a4dd85de18192f99f

--- device.mk
+++ device.mk
@@ -170,16 +170,19 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl-qti \
+    android.hardware.gnss@1.0-service-qti \
+    gps.msm8952 \
     libcurl \
     libgnss \
     libgnsspps
 
 PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/gps/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
-    $(DEVICE_PATH)/gps/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
-    $(DEVICE_PATH)/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
-    $(DEVICE_PATH)/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
-    $(DEVICE_PATH)/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
+    $(DEVICE_PATH)/gps/etc/flp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/flp.conf \
+    $(DEVICE_PATH)/gps/etc/gps.conf:$(TARGET_COPY_OUT_VENDOR)/etc/gps.conf \
+    $(DEVICE_PATH)/gps/etc/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
+    $(DEVICE_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
+    $(DEVICE_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
+    $(DEVICE_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
 # HIDL
 PRODUCT_PACKAGES += \
