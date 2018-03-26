a6dfdd5456661bdfc46e45a53927da1d9f10ca5d
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-26 16:48:25 -0400

    rimo02a: Use QTI bluetooth HAL instead of AOSP
    Change-Id: Ib9aeac7ad7dbfc078ec698f2a2eae360a00ef535

--- device.mk
+++ device.mk
@@ -58,7 +58,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml
 
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
