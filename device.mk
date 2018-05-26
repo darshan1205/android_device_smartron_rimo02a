b26f3bd0a20576f7994eac1bf9ef4149a9111be6
Darshan Manjrekar <darshanmanjrekar1205@gmail.com>
2018-05-26 15:50:12 +0530

    rimo02a: Update audio configs
    Package Version: Smartron/srtphone/rimo02a:8.0.0/T5524INDURC-475/475:user/release-keys
    
    Change-Id: Ib550a7387c76eca6a30ff3e2d50350937619d4bf

--- device.mk
+++ device.mk
@@ -41,9 +41,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths_qrd_skun.xml:system/etc/mixer_paths_qrd_skun.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
-    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
-    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9330.xml:system/etc/sound_trigger_mixer_paths_wcd9330.xml \
-    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9335.xml:system/etc/sound_trigger_mixer_paths_wcd9335.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
 
 PRODUCT_COPY_FILES += \
